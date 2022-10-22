9cd9afc8f1e24b3f09ae87bbb213a60b151e0947
antmorera <ant.morera.lb@gmail.com>
2022-10-22 15:07:12 -0600

    Created the class director

---
+++ game/director.py
@@ -0,0 +1,50 @@
+from game.terminal_Service import Terminal_Service
+from game.parachute import Jumper
+from game.PuzzleWord import Puzzle_Word
+
+class Director:
+
+    def __init__(self):
+
+        self.terminal_service = Terminal_Service()
+        self.parachute = Jumper()
+        self.keep_playing = True
+        self.puzzleword = Puzzle_Word()
+        self.Letter_used = ""
+
+    def start_game(self):
+
+        while self.keep_playing:
+
+            self.do_outputs()
+            self.get_inputs()
+            self.do_updates()
+
+    def do_outputs(self):
+
+        self.puzzleword.draw_word()
+        self.parachute.draw_parachute()
+
+    def get_inputs(self):
+
+        self.Letter_used= self.terminal_service.read("Guess a letter: ")
+
+    def do_updates(self):
+
+        correct_guess = self.puzzleword.process_guess(self.Letter_used)
+        if correct_guess == False:
+            self.parachute.remove_parachute_part()
+        
+        if self.puzzleword.keep_playing() == True:
+        
+            if self.parachute.keep_playing() == True:
+                self.keep_playing == True
+
+            else:
+
+                self.keep_playing = False
+                self.parachute.change_parachute()
+                self.parachute.draw_parachute()
+        
+        else:
+            self.keep_playing = False
