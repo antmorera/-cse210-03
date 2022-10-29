5de640857b1e60bb70ed28d274b79531a5707dcb
Montoya, Alan <gazalem@hotmail.com>
2022-10-29 11:03:11 -0500

    UPDATED DRAFT Director Class

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -1,48 +1,70 @@
-from game.parachute import Parachute
-from game.word_slicer import WordSlicer
+from game.parachutetracker import ParachuteTracker
+from game.wordslicer import WordSlicer
+from game.gametitle import GameTitle
+
 
 class Director:
+    """This class will follow the flow of the game
+    """
 
-    def __init__(self):
+    def __init__(self) -> None:
+        self._sliced_word = WordSlicer()
+        self._parachuteTracker = ParachuteTracker()
+        self._game_title = GameTitle()
+        self._guessed = False
+        self._user_guess = ""
+        self._user_guesses = []
+        self._tries = 5
+        self._guess_word = self._sliced_word.slicer()
 
-        self.parachute = Parachute()
-        self.keep_playing = True
-        self.puzzleword = PuzzleWord()
-        self.Letter_used = ""
+    def startGame(self):
+        """Starts the game by running the main game loop"""
+        pass
 
-    def start_game(self):
+    def _display(self):
+        """Display in the console the flow of the
+        game for inputs and outputs.
+        """
+        pass
 
-        while self.keep_playing:
+    # Logic of the game
+    def _wordTracker(self):
+        """This Method will evaluate the word and the letter
+        that the user what to guess from the jumper
+        Also it update the jumper graph
+        """
+        while not self._guessed:
+            self._display()
+            # print(f"\nUncovered word: {''.join(self._guess_word)}\n\n")
+            self._parachuteTracker.parachuteChooser(self._tries)
+            # Word exchange logic
+            for letter in self._guess_word:
+                if letter in self._user_guesses:
+                    print(letter, end=" ")
+                else:
+                    print("_", end=" ")
 
-            self.do_outputs()
-            self.get_inputs()
-            self.do_updates()
+            self._user_guess = input("\n\n\nPlease guess a letter: ").upper()
+            self._user_guesses.append(self._user_guess.upper())
 
-    def do_outputs(self):
+            if self._user_guess.upper() not in self._guess_word:
+                self._tries -= 1
+                if self._tries == 0:
+                    break
 
-        self.puzzleword.randomWord()
-        self.parachute.print_parachute()
-
-    def get_inputs(self):
-
-        self.Letter_used= self.terminal_service.read("Guess a letter: ")
-
-    def do_updates(self):
-
-        correct_guess = self.puzzleword.process_guess(self.Letter_used)
-        if correct_guess == False:
-            self.parachute.pop_list()
+            self._guessed = True
+                        
+            for letter in self._guess_word:
+                if letter not in self._user_guesses:
+                    self._guessed = False
         
-        if self.keep_playing() == True:
-        
-            if self.parachute.keep_playing() == True:
-                self.keep_playing == True
-
-            else:
-
-                self.keep_playing = False
-                self.parachute.change_parachute()
-                self.parachute.print_parachute()
-        
+        if self._guessed:
+            self._gameWin()
         else:
-            self.keep_playing = False
+            self._gameOver()
+
+
+    def _gameOver(self):
+        pass
+    def _gameWin(self):
+        pass
