9ea2371d8a76e8cec84e11a27647c408d3c52a80
reyarmenta <reyarmenta1@hotmail.com>
2022-10-28 22:40:58 -0600

    Fixed error on the word_slicer class

--- jumper/game/word_slicer.py
+++ jumper/game/word_slicer.py
@@ -5,6 +5,6 @@ class WordSlicer:
     def _init(self):
         self._word = Puzzle_Word().randomWord()
 
-    def slicer(self):list
+    def slicer(self):
         self.sliced_word = list(self._word)
         return self.sliced_word
